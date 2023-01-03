b532d62a4dbe50cd59c675d9caf6716035d45965
soyedarif <soyedariff@gmail.com>
2023-01-03 19:50:48 +0600

    another way of the test

--- script.js
+++ script.js
@@ -276,12 +276,16 @@ console.log(drink2)
 console.log(`I would like to drink ${age >=18?'wine':'water'}`)
 */
 const bill=275;
+/*
 let tip;
 if (bill>=50&&bill<=300){
     tip=(bill*15)/100;
 }else{
     tip=(bill*20)/100;
 }
-
-console.log(tip);
-console.log(`The bill was ${bill}, the tip was ${(bill>=50&&bill<=300)?tip=(bill*15)/100:tip=(bill*20)/100}, and the total value ${bill+tip}`);
+*/
+// // console.log(tip);
+// console.log(`The bill was ${bill}, the tip was ${(bill>=50&&bill<=300)?tip=(bill*15)/100:tip=(bill*20)/100}, and the total value ${bill+tip}`);
+const tip=bill<=300&&bill>=50?bill*0.15:bill*0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+    ${bill+tip}`);
